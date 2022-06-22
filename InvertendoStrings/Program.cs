ec9689663413a5c03084415e7265d08e9ffdf070
Perk <natspindola@hotmail.com>
2022-06-22 19:50:47 -0300

    Correção namespace

--- InvertendoStrings/Program.cs
+++ InvertendoStrings/Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
 
-public static class Program
+namespace InvertendoStrings
 {
-    public static string InverteString(this string str)
+    public static class Program
     {
-
-        char[] chars = str.ToCharArray();
-
-        for (int i = 0; i < str.Length / 2; i++)
+        public static string InverteString(this string str)
         {
-            char ch = chars[i];
-            chars[i] = chars[str.Length - i - 1];
-            chars[str.Length - i - 1] = ch;
+
+            char[] chars = str.ToCharArray();
+
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                char ch = chars[i];
+                chars[i] = chars[str.Length - i - 1];
+                chars[str.Length - i - 1] = ch;
+            }
+
+            return new string(chars);
         }
 
-        return new string(chars);
-    }
-
-    public static void Main()
-    {
-        string texto = "Olá, eu sou a Natália e busco um estágio!";
-        Console.WriteLine("Olá, eu sou a Natália e busco um estágio!");
-        string reverse = texto.InverteString();
-        Console.WriteLine(reverse);
+        public static void Main()
+        {
+            string texto = "Olá, eu sou a Natália e busco um estágio!";
+            Console.WriteLine("Olá, eu sou a Natália e busco um estágio!");
+            string reverse = texto.InverteString();
+            Console.WriteLine(reverse);
+        }
     }
 }
